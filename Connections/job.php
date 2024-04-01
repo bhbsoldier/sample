cf20add3542755606bec767056962571fd875d0f
bhbsoldier <164332556+bhbsoldier@users.noreply.github.com>
2024-04-01 09:55:05 +0530

    Add files via upload

---
+++ Connections/job.php
@@ -0,0 +1,15 @@
+<?php
+# FileName="Connection_php_mysql.htm"
+# Type="MYSQL"
+# HTTP="true"
+$hostname_job = "localhost";
+$database_job="sourcecodester_jobportaldb";
+$username_job = "root";
+$password_job = "";
+$job;
+$database_job = mysqli_connect($hostname_job, $username_job, $password_job,"sourcecodester_jobportaldb") or trigger_error(mysqli_error($database_job),E_USER_ERROR);
+
+
+
+
+
